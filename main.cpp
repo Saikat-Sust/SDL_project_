662184970d6298976480fd205e53af006eb96780
Md saikat <111490728+Saikat-Sust@users.noreply.github.com>
2024-01-27 18:46:37 +0600

    feature added  main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
 #include<bits/stdc++.h>
 #include<math.h>
 #include<SDL2/SDL.h>
+#include<SDL2/SDL_ttf.h>
 using namespace std;
+
+
 int foodX;
 int foodY;
 int direction=1;
 int score=0;
 int rectenglex=100;
 int rectengley=100;
+int bfoodX, bfoodY, bonusScr=0;
+int timex, timey;
+bool redo=false;
+
 vector<pair<int,int>>snakebody;
 vector<pair<int,int>>snakebody1;
+
 SDL_Event event;
 SDL_Window* window=SDL_CreateWindow("Snake Game",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,800,600,SDL_WINDOW_SHOWN);
 SDL_Renderer* renderer=SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+TTF_Font* font;
 bool quit=false;
+
 void foodpos()
 {
     cout<<"score:"<<score++<<endl;
@@ -36,10 +46,25 @@ void foodpos()
          snakebody.push_back( make_pair(rectenglex,rectengley-10) );
     }
 }
+
+void bonusfoodpos()
+{
+    bfoodX = rand() % (780 /10 ) * 10;
+    bfoodY = rand() % (580 / 10) * 10;
+    bonusScr=score;
+    timey=SDL_GetTicks();
+}
+
 void DrawRect(int x, int y) {
     SDL_Rect rect = { x, y, 10, 10};
     SDL_RenderFillRect(renderer, &rect);
 }
+
+void DrawObstracleRect(int x, int y) {
+    SDL_Rect rect = { x, y, 30, 400};
+    SDL_RenderFillRect(renderer, &rect);
+}
+
 void nextdirection()
 {
         switch (event.key.keysym.sym)
@@ -62,6 +87,12 @@ void nextdirection()
                         break;}
 
     } }
+
+    void drawObstracle()
+    {
+        DrawObstracleRect(300, 100);
+        DrawObstracleRect(470, 100);
+    }
     void Drawfood()
     {
         int radius=10;
@@ -83,6 +114,29 @@ void nextdirection()
    else if(((foodX-rectenglex)<=10&&(foodX-rectenglex)>=0)&&((foodY-rectengley)>=0&&(foodY-rectengley)<=20))
         foodpos();
     }
+
+    void Drawbfood()
+    {
+        int radius=10;
+        while(radius--)
+        for(int angle=0;angle<360;angle++)
+        {
+            int x=bfoodX+radius*cos(angle*M_PI/180);
+            int y=bfoodY+radius*sin(angle*M_PI/180);
+            SDL_RenderDrawPoint(renderer,x,y);}
+        }
+    void bfoodcollision()
+    {
+         if(((bfoodX-rectenglex)<=20&&(bfoodX-rectenglex)>=0)&&((rectengley-bfoodY)>=0&&(rectengley-bfoodY)<=10))
+         {bfoodX=-20; bfoodY= -20; score+=10;}
+  else if(((bfoodX-rectenglex)<=20&&(bfoodX-rectenglex)>=0)&&((bfoodY-rectengley)>=0&&(bfoodY-rectengley)<=20))
+         {bfoodX=-20; bfoodY= -20; score+=10;}
+  else if(((rectenglex-bfoodX)<=10&&(rectenglex-bfoodX)>=0)&&((bfoodY-rectengley)>=0&&(bfoodY-rectengley)<=20))
+        {bfoodX=-20; bfoodY= -20; score+=10;}
+   else if(((bfoodX-rectenglex)<=10&&(bfoodX-rectenglex)>=0)&&((bfoodY-rectengley)>=0&&(bfoodY-rectengley)<=20))
+        {bfoodX=-20; bfoodY= -20; score+=10;}
+    }
+
  void snakehead()
     {
        if(direction==1)
@@ -101,7 +155,8 @@ void nextdirection()
    { rectengley-=10;
    if(rectengley==0)
    rectengley=590;}
-    }
+
+  }
   void movebody()
   {   snakehead();
     snakebody1=snakebody;
@@ -111,6 +166,18 @@ void nextdirection()
     for(int i=snakebody.size()-1;i>0;i--)
     { snakebody[i-1]=snakebody1[i];
         DrawRect(snakebody[i].first,snakebody[i].second);
+    }
+
+  }
+
+  void obstracleCollision()
+  {
+    if (rectenglex>=300 && rectenglex<330 && rectengley>=100 && rectengley<500 ||
+    rectenglex>=470 && rectenglex<500 && rectengley>=100 && rectengley<500)
+    {
+        cout<<"GAME IS OVER"<<endl;
+        
+        quit=true;
     }
 
   }
@@ -130,6 +197,16 @@ void nextdirection()
           }
 
      }
+    
+    void renderScore()
+    {
+        string scr = "Your Score: ";
+        scr+=to_string(score);
+        SDL_Surface* s = TTF_RenderText_Solid(font, scr.c_str(), {0, 255, 0});
+        SDL_Rect scrR = {400-s->w/2, 6, s->w, s->h};
+        SDL_Texture* scrT = SDL_CreateTextureFromSurface(renderer, s);
+        SDL_RenderCopy(renderer, scrT, NULL, &scrR);
+    }
 int main(int argc,char* argv[])
 {
     if(SDL_Init(SDL_INIT_VIDEO)!=0)
@@ -137,38 +214,61 @@ int main(int argc,char* argv[])
         cout<<"SDL_Init Error"<<endl;
         return 1;
     }
+    TTF_Init();
+    font = TTF_OpenFont("Bakery.ttf", 24);
  snakebody.push_back( make_pair(rectenglex,rectengley) );
  snakebody.push_back( make_pair(rectenglex+10,rectengley) );
+ //snakebody.push_back( make_pair(rectenglex+20,rectengley) );
  rectenglex+=10;
  foodpos();
+ bfoodX=-20;
+ bfoodY=-20;
+ timex=SDL_GetTicks();
 while(quit!=true)
 {
     while(SDL_PollEvent(&event))
     {
         if(event.type==SDL_QUIT)
-{
-    quit=true;
-}
-     else if(event.type == SDL_KEYDOWN)
-     nextdirection();
+    {
+        quit=true;
     }
+    else if(event.type == SDL_KEYDOWN)
+        nextdirection();
+    }
+    if(timex-timey>=3000) {bfoodX=-20; bfoodY= -20; timex=timey;}
+    if (score%7==0 && score!=bonusScr) bonusfoodpos();
+    if (foodX>=300 && foodX<330 && foodY>=100 && foodY<500 ||
+    foodX>=470 && foodX<500 && foodY>=100 && foodY<500)
+    {
+        redo=true;
+    }
+    if (redo) foodpos();
+
     SDL_SetRenderDrawColor(renderer,0,0,0,255);
     SDL_RenderClear(renderer);
+    renderScore();
+    SDL_SetRenderDrawColor(renderer,10,0,200,255);
+    Drawbfood();
+    bfoodcollision();
     SDL_SetRenderDrawColor(renderer,10,100,200,255);
     Drawfood();
+    SDL_SetRenderDrawColor(renderer,255,100,0,255);
     foodcollision();
     movebody();
+    SDL_SetRenderDrawColor(renderer,255,100,200,255);
+    drawObstracle();
     snakecollision();
-         SDL_RenderPresent(renderer);
-          if(score<=10)
+    obstracleCollision();
+    SDL_RenderPresent(renderer);
+        if(score<=10)
           SDL_Delay(70);
-           else if(score>10&&score<=20)
+        else if(score>10&&score<=20)
           SDL_Delay(60);
-       else if(score>20&&score<=30)
+        else if(score>20&&score<=30)
           SDL_Delay(50);
-          else if(score>30&&score<=40)
+        else if(score>30&&score<=40)
           SDL_Delay(40);
-          else
+        else
           SDL_Delay(30);
 }
  SDL_DestroyRenderer(renderer);
